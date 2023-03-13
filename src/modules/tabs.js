8925e4a26df8215dea96a949671c102e90a043cf
antonborod1n <antonborodin86@gmail.com>
2023-03-13 22:07:36 +0300

    build

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -1,39 +1,43 @@
 document.addEventListener('DOMContentLoaded', function () {
     //tabs
-    const tabsBtn = document.querySelectorAll('.activity__tabs-item');
-    const tabsContent = document.querySelectorAll('.activity__content');
-    const tabsControl = document.querySelector('.activity__tabs');
-    const active = 'active';
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
+    function tabs(btn, content, control) {
+        const tabsBtn = document.querySelectorAll(btn);
+        const tabsContent = document.querySelectorAll(content);
+        const tabsControl = document.querySelector(control);
+        const active = 'active';
 
-        tabsBtn.forEach(item => {
-            item.classList.remove(active);
-        });
-    }
+        function hideTabContent() {
+            tabsContent.forEach(item => {
+                item.style.display = 'none';
+            });
 
-    function showTabsContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabsBtn[i].classList.add(active);
-    }
-
-    hideTabContent();
-    showTabsContent();
-
-    tabsControl.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains('activity__tabs-item')) {
-            tabsBtn.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabsContent(i);
-                    console.log(i)
-                }
+            tabsBtn.forEach(item => {
+                item.classList.remove(active);
             });
         }
-    });
+
+        function showTabsContent(i = 0) {
+            tabsContent[i].style.display = 'block';
+            tabsBtn[i].classList.add(active);
+        }
+
+        hideTabContent();
+        showTabsContent();
+
+        tabsControl.addEventListener('click', (event) => {
+            const target = event.target;
+
+            if (target && target.classList.contains('activity__tabs-item')) {
+                tabsBtn.forEach((item, i) => {
+                    if (target == item) {
+                        hideTabContent();
+                        showTabsContent(i);
+                    }
+                });
+            }
+        });
+    }
+
+    tabs('.activity__tabs-item', '.activity__content', '.activity__tabs');
 })
