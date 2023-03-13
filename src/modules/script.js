bba641664789259135814b6b6f6a0c011b9d0229
antonborod1n <antonborodin86@gmail.com>
2023-03-13 15:07:04 +0300

    added slider adaptive

--- src/modules/script.js
+++ src/modules/script.js
@@ -5,26 +5,25 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 document.addEventListener('DOMContentLoaded', function () {
+    const slider = document.querySelector('.slider-main');
+    const sliderNav = document.querySelector('.slider-nav');
+
     const swiper = new Swiper('.team-slider', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        slidesPerView: 'auto',
+        spaceBetween: 16,
+        loop: true,
+        freeMode: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
         },
     });
 
-    const offerSlider = new Swiper('.offer-slider', {
-        slidesPerView: 5,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
     const teamSlider = new Swiper('.team__gallery-inner', {
         slidesPerView: 5,
         spaceBetween: 10,
-
+        grabCursor: true,
         breakpoints: {
             350: {
                 slidesPerView: 1,
@@ -48,17 +47,82 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    /* let menuMain = document.querySelector('.header__bottom');
-    let menuBtn = document.querySelector('.menu__btn');
-    let close = document.querySelector('.menu__close');
+    const offerSlider = new Swiper('.offer-slider', {
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+        breakpoints: {
+            350: {
+                slidesPerView: 1,
+            },
 
-    menuBtn.addEventListener('click', function () {
-        menuMain.classList.add('active');
-        close.classList.add('active');
+            550: {
+                slidesPerView: 2,
+            },
+
+            850: {
+                slidesPerView: 3,
+            },
+
+            1250: {
+                slidesPerView: 4,
+            },
+
+        }
     });
 
-    close.addEventListener('click', function () {
-        menuMain.classList.remove('active');
-        close.classList.remove('active');
-    }); */
+    const activitySlider = new Swiper('.activity-slider', {
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+    let mySwiperNav = new Swiper(sliderNav, {
+        slidesPerView: 5,
+        spaceBetween: 10,
+        loopedSlides: 5,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        direction: 'vertical',
+    })
+
+    let mySwiper = new Swiper(slider, {
+        spaceBetween: 10,
+        loopedSlides: 5,
+        thumbs: {
+            swiper: mySwiperNav,
+        },
+
+        breakpoints: {
+            250: {
+                slidesPerView: 1.2,
+            },
+
+            768: {
+                slidesPerView: 1,
+            },
+        }
+    })
+
+    const sliderMains = new Swiper('.main-slider', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 3000,
+        },
+    });
 })
